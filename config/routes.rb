215ecda08a2745b0e234b72baf54975fca9e8b8e
Hauru427 <hauru.051427@gmail.com>
2024-06-08 14:21:41 +0900

    User singup setting model,controller,route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get '/signup', to: 'users#new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "static_pages#top"
+  resources :users, only: %i[new create]
 end
