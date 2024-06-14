c24e42b9a98fb10282d4cd2879cd1205a4949577
Hauru427 <hauru.051427@gmail.com>
2024-06-14 17:38:16 +0900

    add due_date of card

--- app/models/card.rb
+++ app/models/card.rb
@@ -3,4 +3,5 @@ class Card < ApplicationRecord
 
   validates :title,  length: { maximum: 255 }
   validates :memo, length: { maximum:1000 }
+  validates :due_date, 
 end
--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :card do
     title { "Test Card" }
     memo { "Test Memo" }
+    due_date { DateTime.now + 1.day }
     association :list
   end
 end
