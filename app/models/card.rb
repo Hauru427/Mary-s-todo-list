cb2ffdc6dd53ba89e01ed1c4db0da3cf3f1b34e3
Hauru427 <hauru.051427@gmail.com>
2024-06-18 13:35:09 +0900

    Fix test of card"

--- app/models/card.rb
+++ app/models/card.rb
@@ -3,5 +3,13 @@ class Card < ApplicationRecord
 
   validates :title,  length: { maximum: 255 }
   validates :memo, length: { maximum:1000 }
-  validates :due_date, 
+  validate :due_date_cannnot_be_in_the_past
+
+  private
+
+  def due_date_cannnot_be_in_the_past
+    if due_date.present? && due_date < DateTime.now
+      errors.add(:due_date, "can't be in the past")
+    end
+  end
 end
