db0ed41ec575ee4ea7bc71140befe099eb46d990
Hauru427 <hauru.051427@gmail.com>
2024-06-20 14:25:39 +0900

    Add gem and line_id to usermodel

--- Gemfile
+++ Gemfile
@@ -58,6 +58,10 @@ gem 'font-awesome-sass'
 # ユーザー認証、ログイン、ログアウト、パスワードリセットに使用
 gem 'sorcery', '0.17.0'
 
+# Lineメッセージのために追加
+gem 'dotenv-rails'
+gem 'line-bot-api'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_14_061648) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_20_051925) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,6 +39,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_14_061648) do
     t.string "user_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "line_id"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
