3919714c09a8d0f9186daabcf138228d1324c364
Hauru427 <hauru.051427@gmail.com>
2024-06-26 16:00:29 +0900

    Add merry's message

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -4,18 +4,51 @@ class RemindersController < ApplicationController
   skip_before_action :require_login, only: %i[callback]
 
   def send_reminders
+    send_normal_reminders
+    send_merry_reminders
+  end
+
+  def send_normal_reminders
     Card.where('due_date <= ?', Time.now + 1.hour).find_each do |card|
       send_line_reminder(card)
     end
   end
 
-  def send_line_reminder(card)
+  def send_normal_line_reminder(card)
     user = card.list.user
 
     return unless user.line_id.present?
     message = {
       type: 'text',
       text: "#{card.title}の期限が近づいています。"
+    }
+
+    response = client.push_message(user.line_id, message)
+    Rails.logger.info("LINE reminder sent: #{response}")
+  end
+
+  def send_merry_reminders
+    Card.where('due_date < ?', Time.now).find_each do |card|
+      days_overdue = (Time.now.to_date - card.due_date.to_date).to_i
+      case days_overdue
+      when 1
+        send_merry_line_reminder(card, "test")
+      when 2
+        send_merry_line_reminder(card, "test2")
+      when 3
+        send_merry_line_reminder(card, "test3")
+        card.destroy
+      end
+    end
+  end
+
+  def send_merry_line_reminder(card, messae_part)
+    user = card.list.user
+
+    return unless user.line_id.present?
+    message = {
+      type: 'text',
+      text: "もしもし、私メリー、#{card.title}がまだ終わってないわね。#{message_part}"
     }
 
     response = client.push_message(user.line_id, message)
