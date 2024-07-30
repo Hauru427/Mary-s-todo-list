ea025dd73d952e6c516036f4511d22b52bda46ca
Hauru427 <hauru.051427@gmail.com>
2024-07-30 21:39:59 +0900

    Create move function

--- app/javascript/react/features/todos/components/List.tsx
+++ app/javascript/react/features/todos/components/List.tsx
@@ -1,4 +1,12 @@
 import React from "react";
+import {
+  DragDropContext,
+  Droppable,
+  Draggable,
+  DroppableProvided,
+  DraggableProvided,
+  DropResult,
+} from 'react-beautiful-dnd';
 import { useCards } from '../hooks/useCards';
 import { List } from '../types';
 import CardCard from "./CardCard";
@@ -32,36 +40,56 @@ export default function List({
 
   return (
     <>
-      {lists.map((list) => (
-        <div key={list.id} className="ms-4 d-flex flex-column border rounded" style={{ maxHeight: '100%', backgroundColor: '#d1d5db' }}>
-          <div className="ticky-top p-2 d-flex justify-content-between align-items-center position-relative">
-            <h5 className="m-0">{list.title}</h5>
-            <ListDropdownMenu
-            lists={lists}
-            setLists={setLists}
-            list={list}
-            cards={cards}
-            setCards={setCards}
-          />
-          </div>
-          <div className="min-vh-100" style={{ width: '350px' }}>
-            {filterdCards(list.id).map((card) => (
-              <div key={card.id} className="m-2">
-                <CardCard
-                  cards={cards}
-                  setCards={setCards}
-                  card={card}
-                />
+      <DragDropContext>
+        {lists.map((list) => (
+          <div key={list.id} className="ms-4 d-flex flex-column border rounded" style={{ maxHeight: '100%', backgroundColor: '#d1d5db' }}>
+            <div className="p-2 d-flex justify-content-between align-items-center position-relative">
+              <h5 className="m-0">{list.title}</h5>
+              <ListDropdownMenu
+                lists={lists}
+                setLists={setLists}
+                list={list}
+                cards={cards}
+                setCards={setCards}
+              />
+            </div>
+            <Droppable droppableId={list.id.toString()}>
+              {(provided: DroppableProvided) => (
+                <div
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                  className="min-vh-100" style={{ width: '350px' }}
+                >
+                  {filterdCards(list.id).map((card,index) => (
+                    <div key={card.id} className="m-2">
+                      <Draggable
+                        key={card.id}
+                        draggableId={card.id.toString()}
+                        index={index}
+                      >
+                        {(provided: DraggableProvided) => (
+                          <CardCard
+                            cards={cards}
+                            setCards={setCards}
+                            card={card}
+                            provided={provided}
+                          />
+                        )}
+                      </Draggable>
+                    </div>
+                ))}
+                {provided.placeholder}
               </div>
-            ))}
+              )}
+            </Droppable>
+            <CardCreateForm
+              cards={cards}
+              setCards={setCards}
+              listId={list.id}
+            />
           </div>
-          <CardCreateForm
-            cards={cards}
-            setCards={setCards}
-            listId={list.id}
-          />
-        </div>
-      ))}
+        ))}
+      </DragDropContext>
     </>
   );
 }
