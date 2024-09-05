8ff854eba560051ee45bbfbe2d3fec9da4669fdc
Hauru427 <hauru.051427@gmail.com>
2024-09-05 16:31:05 +0900

    Setting environment test of react

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  reporters: ["default", ["jest-junit", { outputDirectory: "tmp", outputName: "result.xml", classNameTemplate: "{filepath}" }]],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/app/javascript/$1',
+  },
+};
